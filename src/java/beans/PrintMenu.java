2bc83181a2e4089b895f3739c8d59a653df27cf5
Joakim Sjöquist <josj1701@student.miun.se>
2020-02-20 14:53:40 +0100

    index & css

---
+++ src/java/beans/PrintMenu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author joaki
+ */
+@Named(value = "printMenu")
+@Dependent
+public class PrintMenu {
+
+    public PrintMenu() {
+        
+    }
+    
+    
+}
