d37f9f7f21589c0604163e64316d81f9e36da79b
mahl1400 <54980558+mahl1400@users.noreply.github.com>
2020-02-20 14:56:18 +0100

    Added a selectListener

--- src/java/beans/CalendarManagedBean.java
+++ src/java/beans/CalendarManagedBean.java
@@ -4,14 +4,15 @@ import java.io.Serializable;
 
 import java.util.Date;
 
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
+import org.primefaces.event.SelectEvent;
 
 @Named(value = "calendarManagedBean")
 @SessionScoped
 public class CalendarManagedBean implements Serializable{
 	private Date date = new Date();
+        private String temp = "test";
         
 	public Date getDate() {
 		return date;
@@ -20,4 +21,14 @@ public class CalendarManagedBean implements Serializable{
 	public void setDate(Date date) {
 		this.date = date;
 	}
+        
+        public void dateSelectedAction(SelectEvent e){
+		Date date = (Date)e.getObject();
+                temp = date.toString();
+//		System.out.println("<p>Date Selected Is ::" + date + "</p>");
+	}
+        
+        public String getTemp() {
+            return temp;
+        }
 }
