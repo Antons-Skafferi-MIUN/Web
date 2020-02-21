979c6218a18191c42e0b9045985f84879672fa5c
joek1705 <49062436+joek1705@users.noreply.github.com>
2020-02-21 08:14:44 +0100

    Add files via upload

--- src/java/General/Lunch.java
+++ src/java/General/Lunch.java
@@ -1,33 +1,40 @@
-
 package General;
 
 public class Lunch {
-    private int weekNumber;
-    private String name;
-    private int ID;
+    private String lunch_id;
+    private String lunch_name;
+    private String lunch_week;
+    private String lunch_day;
 
-    public int getWeekNumber() {
-        return weekNumber;
+    public String getLunch_id() {
+        return lunch_id;
     }
 
-    public void setWeekNumber(int weekNumber) {
-        this.weekNumber = weekNumber;
+    public void setLunch_id(String lunch_id) {
+        this.lunch_id = lunch_id;
     }
 
-    public String getName() {
-        return name;
+    public String getLunch_name() {
+        return lunch_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setLunch_name(String lunch_name) {
+        this.lunch_name = lunch_name;
     }
 
-    public int getID() {
-        return ID;
+    public String getLunch_week() {
+        return lunch_week;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
+    public void setLunch_week(String lunch_week) {
+        this.lunch_week = lunch_week;
     }
-    
+
+    public String getLunch_day() {
+        return lunch_day;
+    }
+
+    public void setLunch_day(String lunch_day) {
+        this.lunch_day = lunch_day;
+    }
 }
