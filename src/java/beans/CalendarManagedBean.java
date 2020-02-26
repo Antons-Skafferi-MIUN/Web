910e040d7e83a436b1f2aaa7c0c6c16659d99f6b
mahl1400 <54980558+mahl1400@users.noreply.github.com>
2020-02-26 09:44:50 +0100

    Added managerBean and updated events layout

--- src/java/beans/CalendarManagedBean.java
+++ src/java/beans/CalendarManagedBean.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Named;
-import org.primefaces.event.SelectEvent;
 
 @Named(value = "calendarManagedBean")
 @Dependent
@@ -21,14 +20,6 @@ public class CalendarManagedBean implements Serializable{
 	public void setDate(Date date) {
 		this.date = date;
 	}
-        
-        public void dateSelectedAction(SelectEvent e){
-		Date date = (Date)e.getObject();
-                setDate(date);
-                temp = "Temptest";
-		System.out.println("Date Selected Is :" + date + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHello");
-                System.out.println(temp);
-        }
         
         public String getTemp() {
             return temp;
